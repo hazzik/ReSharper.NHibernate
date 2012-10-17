3f5a7a800094f431eda9d467df6cd30ae2851093
Alexander I. Zaytsev <hazzik@gmail.com>
2012-10-18 02:51:51 +0600

    Add HbmXmlLanguage

---
+++ NHibernatePlugin/LanguageService/HbmXmlLanguage.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Xml;
+
+namespace NHibernatePlugin.LanguageService
+{
+    [LanguageDefinition(Name)]
+    public class HbmXmlLanguage : XmlLanguage
+    {
+        public new const string Name = "HBM_XML";
+
+        [CanBeNull]
+        public static readonly HbmXmlLanguage Instance;
+
+        private HbmXmlLanguage()
+            : base(Name, "HbmXml") {
+        }
+
+        protected HbmXmlLanguage([NotNull] string name)
+            : base(name) {
+        }
+
+        protected HbmXmlLanguage([NotNull] string name, [NotNull] string presentableName)
+            : base(name, presentableName) {
+        }
+    }
+}
